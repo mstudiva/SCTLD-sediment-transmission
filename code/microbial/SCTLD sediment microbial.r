798acfd8fb5c7853847df0dec057a99f6ef581ed
mstudiva <studivanms@gmail.com>
2021-10-25 23:21:33 -0400

    sample IDs corrected

--- code/microbial/SCTLD sediment microbial.r
+++ code/microbial/SCTLD sediment microbial.r
@@ -1,3 +1,8 @@
+#### packages ####
+
+# BiocManager::install("phyloseq")
+# install.packages("corncob")
+
 library(corncob)
 library(phyloseq)
 library(magrittr)
@@ -10,18 +15,17 @@ library(cowplot)
 library(grid)
 library(scales)
 library(RColorBrewer)
-library(ggplot)
 
+
+#### data import ####
 
 asv <- read.table("ASV_table.txt", sep = "\t", header = TRUE, row.names = 1)
 taxa <- as.matrix(read.table("taxa.txt", sep = "\t", header = TRUE, row.names = 1))
 metadata <- read.table("metadata.txt", header = TRUE,row.names = 'sample_id')
 asv <- t(asv)
 
-#order the metadata so it matches the ASV table
-
+# order the metadata so it matches the ASV table
 idx <- match(rownames(asv),rownames(metadata))
-
 metadata <- metadata[idx,]
 
 ASV = otu_table(asv, taxa_are_rows = FALSE)
@@ -31,23 +35,23 @@ META = sample_data(metadata)
 ps <- phyloseq(ASV, TAX, META)
 
 length(get_taxa_unique(ps, "Family")) 
-
-#number of unique family samples = 313
+# number of unique family samples = 313
  
 length(get_taxa_unique(ps, "Order")) 
-
-#number of unique order samples = 213
+# number of unique order samples = 213
 
 get_taxa_unique(ps, "Phylum") 
 
-#corncob-different test 
 
+#### statistical tests ####
+
+# corncob differencial abundance test 
 set.seed(1)
 
-da_analysis <- differentialTest(formula = ~ fate,
-	phi.formula = ~ fate,
+da_analysis <- differentialTest(formula = ~ condition,
+	phi.formula = ~ condition,
 	formula_null = ~ 1,
-	phi.formula_null = ~ fate,
+	phi.formula_null = ~ condition,
 	test = "Wald", boot = FALSE,
 	data = ps,
 	fdr_cutoff = 0.05)
@@ -56,14 +60,13 @@ da_analysis
 da_analysis$significant_taxa 
 plot(da_analysis)
 
-#to create genus-level-table
-
+# create genus-level-table
 ps_genus <- ps %>% tax_glom("Genus")
 
-da_genus <- differentialTest(formula = ~ fate,
-	phi.formula = ~ fate,
+da_genus <- differentialTest(formula = ~ condition,
+	phi.formula = ~ condition,
 	formula_null = ~ 1,
-	phi.formula_null = ~ fate,
+	phi.formula_null = ~ condition,
 	test = "Wald", boot = FALSE,
 	data = ps_genus,
 	fdr_cutoff = 0.05)
@@ -71,40 +74,41 @@ da_genus <- differentialTest(formula = ~ fate,
 plot(da_genus)
 
 
-#to generate Figure 5 
+#### bubble plot ####
 
 sign.genera <- read.table("sign.genera.txt", sep = "\t", header = TRUE)
 
-#convert data frame from a "wide" format to a "long" format
-
+# convert data frame from a "wide" format to a "long" format
 sign.genera.long = melt(sign.genera, id = c("Sample"))
+sign.genera.long <- sign.genera.long %>% separate(Sample, 
+                c("Sample", "Treatment","Condition"))
 
-#pick colors (you need 16 distinct colors) 
-
-colours = c("#833FE2", "#DD579C", "#CEA984", "#8DAAE4", 
+# pick colors (you need 16 distinct colors) 
+colors = c("#833FE2", "#DD579C", "#CEA984", "#8DAAE4", 
 			"#8ADF97", "#DF7573", "#836678", "#E4CED1", 
 			"#7E74D5", "#73E2CF", "#E8924A", "#CBE545", 
 			"#70B8C6", "#DBA9D3", "#C7E5E6", "#72E356")
 						
-show_col(colours)
+show_col(colors)
 
-#keep the order of samples from your data
-
-sign.genera.long$Sample <- factor(sign.genera.long$Sample,levels=unique(sign.genera.long$Sample))
+# keep the order of samples from your data
+sign.genera.long$Condition <- factor(sign.genera.long$Condition,levels=unique(sign.genera.long$Condition))
  
 xx = ggplot(sign.genera.long, aes(x = Sample, y = variable)) + 
   geom_point(aes(size = value, fill = variable), alpha = 0.75, shape = 21) + 
-  scale_size_continuous(limits = c(0, 7), range = c(0,17), breaks = c(0.5,5)) + 
+  scale_size_continuous(limits = c(0, 7), range = c(0,17), breaks = c(0.5,2.5,5)) + 
+  facet_grid(. ~ Condition + Treatment, scales="free", space="free") +
   labs( x= "", y = "", size = "Relative Abundance (%)", fill = "")  + 
   theme(legend.key=element_blank(), 
-  axis.text.x = element_text(colour = "black", size = 12, face = "bold", angle = 90, vjust = 0.3, hjust = 1), 
-  axis.text.y = element_text(colour = "black", face = "bold", size = 12), 
-  legend.text = element_text(size = 10, face ="bold", colour ="black"), 
+  axis.text.x = element_text(color = "black", size = 12, face = "bold", angle = 90, vjust = 0.3, hjust = 1), 
+  axis.text.y = element_text(color = "black", face = "bold", size = 12), 
+  legend.text = element_text(size = 10, face ="bold", color ="black"), 
   legend.title = element_text(size = 12, face = "bold"), 
-  panel.background = element_blank(), panel.border = element_rect(colour = "black", fill = NA, size = 1.2), 
+  panel.background = element_blank(), panel.border = element_rect(color = "black", fill = NA, size = 1.2), 
+  panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
   legend.position = "top",legend.justification="left", legend.direction = "horizontal") +  
-  scale_fill_manual(values = colours, guide = FALSE) + 
+  scale_fill_manual(values = colors, guide = "none") + 
   scale_y_discrete(limits = rev(levels(sign.genera.long$variable))) 
-  
 xx
 
+ggsave("microbial_abundance.pdf", plot= xx, width=10, height=8, units="in", dpi=300)
--- code/transmission/SCTLD sediment transmission.R
+++ code/transmission/SCTLD sediment transmission.R
@@ -1,16 +1,24 @@
+#### packages ####
+
 library(ggplot2)
 library(ggpubr)
 library(rcompanion)
 library(MASS)
+library(stringr)
 
-####data import####
+
+#### data import ####
+
 sed <- read.csv("sctld_sediment.csv", head=T)
+sed$Genotype <- as.factor(sed$Genotype)
 str(sed)
 head(sed)
 # keeps species/treatment order as imported
 sed$Species.Treatment=factor(sed$Species.Treatment, levels=unique(sed$Species.Treatment)) 
 
-####data normality/transformation####
+
+#### data normality/transformation ####
+
 # plots histogram and q-q plot for test of normality assumptions
 hist(sed$total.days)
 qqnorm(sed$total.days)
@@ -20,17 +28,19 @@ qqline(sed$total.days)
 shapiro.test(sed$total.days)
 # not normal
 
-#BoxCox transformation finds best exponent value for data transformation
+# BoxCox transformation finds best exponent value for data transformation
 bc <- boxcox(sed$total.days ~ sed$Species.Treatment)
 lambda <- bc$x[which.max(bc$y)]
 
-####statistical tests####
+
+#### statistical tests ####
+
 # ANOVA on untransformed data
-anova <- aov (total.days ~ Species*Treatment, data =sed)
+anova <- aov (total.days ~ Species*Genotype*Treatment, data =sed)
 summary(anova)
 
 # ANOVA on Box-Cox transformed data
-anova.bc <- aov(((total.days^lambda-1)/lambda) ~ Species*Treatment, data=sed)
+anova.bc <- aov(((total.days^lambda-1)/lambda) ~ Species*Genotype*Treatment, data=sed)
 summary(anova.bc)
 
 # Q-Q plot comparison between untransformed and transformed data
@@ -45,18 +55,20 @@ qqnorm(anova.bc$residuals); qqline(anova.bc$residuals)
 tukey <- TukeyHSD(anova)
 tukey
 
+# creating a dataframe of the pairwise comparisons needed for plots and doing a bit of table reformatting
 letters <- data.frame(tukey$`Species:Treatment`)
-# need to export table for reformatting
-write.csv(letters, file = "post hoc results.csv")
-# add column label 'comparison' to first column, and find/replace all ':' with '.'
-letters <- read.csv("post hoc results.csv", head = T)
+letters$Var <- rownames(letters)
+names(letters)[5] <- "comparison"
+letters$comparison = str_replace_all(letters$comparison,":",".")
 letters
 
 # creates compact letter display of significant pairwise differences for figure
 cld <- cldList(p.adj ~ comparison, data = letters, threshold = 0.05)
 cld
 
-####time to transmission figure####
+
+#### time to transmission figure ####
+
 sed$Species.Treatment=factor(sed$Species.Treatment, levels=unique(sed$Species.Treatment)) 
 # boxplots comparing time to transmission among species/treatments
 transmission <-
@@ -65,15 +77,15 @@ transmission <-
     x = "Species.Treatment",
     y = "total.days",
     color = "grey30",
-    # palette = c("cornflowerblue", "orange2"),
-    fill = "Species",
+    palette = c("#EBED9D", "#868659","#CECE88","#EBED9D", "#868659","#CECE88"),
+    fill = "Species.Treatment",
     add = "jitter",
     add.params = list(size = 1, jitter = 0.5),
     width = 0.7,
     size = 0.5
   ) + labs(x = "Treatment",
            y = "Days",
-           fill = 'Species') + 
+           fill = 'Treatment') + 
   theme_bw() + 
   theme(plot.title = element_text(hjust = 0.5),  legend.position = "right")+
   geom_text(data=cld, aes(x = Group, y=0, label=Letter)) 
@@ -81,12 +93,14 @@ transmission
 
 ggsave("time_to_transmission.pdf", plot= transmission, width=6, height=4, units="in", dpi=300)
 
-####transmission rate figure####
+
+#### transmission rate figure ####
+
 rate <- read.csv("transmission_rate.csv", head = T)
 rate
 rate$treatment=factor(rate$treatment, levels=unique(rate$treatment)) 
 transrate <- ggplot(rate, aes(fill=forcats::fct_rev(condition), y=rate, x=treatment)) + 
-   scale_fill_manual(values=c("#F8766d","#F8766d")) +
+   scale_fill_manual(values=c("#EBED9D", "#EBED9D")) +
   geom_col(width = 0.5) +
   theme_bw() 
 transrate
